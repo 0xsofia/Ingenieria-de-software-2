30015a4ad064f14d7aba8e4a042ab9fe232bea2e
Lautaro Perone <lautyperone@hotmail.com>
2026-05-14 23:36:15 -0300

    corrijo imports

--- App-Centro-De-Actividades/Backend/src/core/models/clase.py
+++ App-Centro-De-Actividades/Backend/src/core/models/clase.py
@@ -1,5 +1,7 @@
-from ...core.database import db
+from src.core.database import db
 class Clase(db.Model):
+    __tablename__ = "clase"
+
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     horario_inicio = db.Column(db.DateTime, nullable=False)
--- App-Centro-De-Actividades/Backend/src/core/models/reserva.py
+++ App-Centro-De-Actividades/Backend/src/core/models/reserva.py
@@ -1,6 +1,8 @@
-from ...core.database import db
+from src.core.database import db
 
 class Reserva(db.Model):
+    __tablename__ = "reserva"
+
     id = db.Column(db.Integer, primary_key=True)
     dni_cliente = db.Column(db.String(20), nullable=False)
     clase_id = db.Column(db.Integer, db.ForeignKey('clase.id'))
