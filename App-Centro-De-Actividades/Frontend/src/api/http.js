6ee276ef5619aaba88fbcc21b502ef28cb6be11b
Lautaro Perone <lautyperone@hotmail.com>
2026-05-22 00:07:21 -0300

    .

--- App-Centro-De-Actividades/Frontend/src/api/http.js
+++ App-Centro-De-Actividades/Frontend/src/api/http.js
@@ -11,7 +11,7 @@ export const http = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': '1',
+    // 'ngrok-skip-browser-warning': '1',
   },
 })
 
