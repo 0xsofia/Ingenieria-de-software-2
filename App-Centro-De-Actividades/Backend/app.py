94b4c20995b3f84148d6141832db751e70b0d211
Lautaro Perone <lautyperone@hotmail.com>
2026-05-21 22:53:41 -0300

    variable de entorno db

--- App-Centro-De-Actividades/Backend/app.py
+++ App-Centro-De-Actividades/Backend/app.py
@@ -7,5 +7,7 @@ app = create_app()
 if __name__ == '__main__':
     use_ssl = (os.environ.get("FLASK_SSL_ADHOC") or "").strip().lower() in {"1", "true", "yes"}
     app.run(ssl_context="adhoc" if use_ssl else None)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
     
 CORS(app)
