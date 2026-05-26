3e7bd30f83f1a675865a16a53e7f2a7f9ffabb32
Lautaro Perone <lautyperone@hotmail.com>
2026-05-25 21:30:29 -0300

    timeout

--- App-Centro-De-Actividades/Frontend/src/api/http.js
+++ App-Centro-De-Actividades/Frontend/src/api/http.js
@@ -7,7 +7,7 @@ console.log('📡 [AXIOS] Conectando a la API en URL:', baseURL);
 
 export const http = axios.create({
   baseURL,
-  timeout: 10000,
+  timeout: 20000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
